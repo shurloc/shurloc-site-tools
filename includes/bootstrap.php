cb480c76afdc522085233928cf067ad342d41ce8
Liam Reimers <liam.reimers@gmail.com>
2026-08-29 13:16:34 -0700

    Wire the media domain into the plugin bootstrap

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -9,14 +9,19 @@ declare( strict_types=1 );
 
 namespace Shurloc\SiteTools;
 
+use Shurloc\SiteTools\Media\Bootstrap as Media_Bootstrap;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
  * Bootstrap the plugin.
+ *
+ * @return void
  */
 function shurloc_site_tools_bootstrap(): void {
+
 	/**
 	 * Autoloader.
 	 */
@@ -28,6 +33,14 @@ function shurloc_site_tools_bootstrap(): void {
 	);
 
 	$autoloader->register();
+
+	/**
+	 * Media domain.
+	 */
+
+	$media_bootstrap = new Media_Bootstrap();
+
+	$media_bootstrap->register();
 }
 
 add_action(
