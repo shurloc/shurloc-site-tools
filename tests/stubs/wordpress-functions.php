0c596b26b945a7cde8fd2370e572c0257da772e6
Liam Reimers <liam.reimers@gmail.com>
2026-08-29 11:46:38 -0700

    Migrate test as SEOServiceTest and add associated infrastructure

--- tests/stubs/wordpress-functions.php
+++ tests/stubs/wordpress-functions.php
@@ -6,3 +6,42 @@
  */
 
 declare( strict_types=1 );
+
+
+/**
+ * Test post meta values.
+ */
+$GLOBALS['shurloc_test_post_meta'] = array();
+
+
+if ( ! function_exists( 'get_post_meta' ) ) {
+	/**
+	 * Retrieve post meta.
+	 *
+	 * @param int    $post_id Post ID.
+	 * @param string $key     Meta key.
+	 * @param bool   $single  Whether to return a single value.
+	 * @return mixed
+	 */
+	function get_post_meta(
+		int $post_id,
+		string $key = '',
+		bool $single = false
+	): mixed {
+		if ( '' === $key ) {
+			return $GLOBALS['shurloc_test_post_meta'][ $post_id ] ?? array();
+		}
+
+		$value = $GLOBALS['shurloc_test_post_meta'][ $post_id ][ $key ] ?? '';
+
+		if ( $single ) {
+			return $value;
+		}
+
+		if ( '' === $value ) {
+			return array();
+		}
+
+		return array( $value );
+	}
+}
