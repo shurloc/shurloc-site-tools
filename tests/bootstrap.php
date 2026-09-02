4d5285f5031644ce98e912b0f6c5de2eaf413097
Liam Reimers <liam@shurloc.com>
2026-09-02 10:16:46 -0700

    Update tests bootstrap for  Mesh_Product_Data_Service_Double and Mesh_Product_Table_Renderer_Double

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -51,6 +51,8 @@ require_once __DIR__ . '/doubles/class-wp-post.php';
 require_once __DIR__ . '/doubles/class-wp-user.php';
 require_once __DIR__ . '/doubles/class-product-catalog-service.php';
 require_once __DIR__ . '/doubles/class-mesh-product-analyzer.php';
+require_once __DIR__ . '/doubles/class-mesh-product-data-service.php';
+require_once __DIR__ . '/doubles/class-mesh-product-table-renderer.php';
 require_once __DIR__ . '/doubles/class-wc-datetime.php';
 require_once __DIR__ . '/doubles/class-wc-order.php';
 require_once __DIR__ . '/doubles/class-wc-product.php';
